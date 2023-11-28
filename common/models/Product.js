df46e84b26de86743e1a9308a4e10869194c4ccb
noklash <123834419+noklash@users.noreply.github.com>
2023-11-28 13:34:15 +0300

    mongo db connection test

--- common/models/Product.js
+++ common/models/Product.js
@@ -1,65 +1,108 @@
-const { DataTypes } = require("sequelize");
+// const { DataTypes } = require("sequelize");
 const { productPriceUnits } = require("../../config");
+const mongoose = require('mongoose');
 
-const ProductModel = {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
+const productSchema = new mongoose.Schema({
   name: {
-    type: DataTypes.STRING,
-    allowNull: false,
+    type: String,
+    required: true
   },
   description: {
-    type: DataTypes.STRING,
-    allowNull: false
+    type: String,
+    required: true
   },
   image: {
-    type: DataTypes.STRING,
-    allowNull: false,
+    type: String,
+    required: true
   },
   price: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
+    type: Number,
+    required: true
   },
   priceUnit: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    defaultValue: productPriceUnits.DOLLAR,
+    type: String,
+    required: true,
+    defaults: productPriceUnits.DOLLAR,
   },
-};
 
+})
+
+
+// const ProductModel = {
+//   id: {
+//     type: DataTypes.INTEGER,
+//     autoIncrement: true,
+//     primaryKey: true,
+//   },
+//   name: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   },
+//   description: {
+//     type: DataTypes.STRING,
+//     allowNull: false
+//   },
+//   image: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   },
+//   price: {
+//     type: DataTypes.INTEGER,
+//     allowNull: false,
+//   },
+//   priceUnit: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//     defaultValue: productPriceUnits.DOLLAR,
+//   },
+// };
 module.exports = {
-  initialise: (sequelize) => {
-    this.model = sequelize.define("product", ProductModel)
-  },
-
+  initialise: (mongoose) => {
+        this.model = mongoose.model('Product', productSchema)
+      },
+  
   createProduct: (user) => {
-    return this.model.create(user);
-  },
-
+        return this.model.create(user);
+      },    
+  
   findProduct: (query) => {
-    return this.model.findOne({
-      where: query,
-    });
-  },
-
-  updateProduct: (query, updatedValue) => {
-    return this.model.update(updatedValue, {
-      where: query,
-    });
-  },
-
-  findAllProducts: (query) => {
-    return this.model.findAll({
-      where: query
-    });
-  },
-
-  deleteProduct: (query) => {
-    return this.model.destroy({
-      where: query
-    });
-  }
+        return this.model.findOne().where(query).exec();
+//TODO=> COMEBACK HERE
+      },
+    
 }
+
+
+// module.exports = {
+//   initialise: (sequelize) => {
+//     this.model = sequelize.define("product", ProductModel)
+//   },
+
+  // createProduct: (user) => {
+  //   return this.model.create(user);
+  // },
+
+  // findProduct: (query) => {
+  //   return this.model.findOne({
+  //     where: query,
+  //   });
+  // },
+
+//   updateProduct: (query, updatedValue) => {
+//     return this.model.update(updatedValue, {
+//       where: query,
+//     });
+//   },
+
+//   findAllProducts: (query) => {
+//     return this.model.findAll({
+//       where: query
+//     });
+//   },
+
+//   deleteProduct: (query) => {
+//     return this.model.destroy({
+//       where: query
+//     });
+//   }
+// }
--- index.js
+++ index.js
@@ -3,11 +3,18 @@ const app = Express();
 const cors = require("cors");
 const morgan = require("morgan");
 
-const { Sequelize } = require("sequelize");
+// const { Sequelize } = require("sequelize");
+const mongoose = require('mongoose')
 
 const { port } = require("./config");
 const PORT = process.env.PORT || port;
 
+async function connectTodb(){
+  await mongoose.connect('mongodb+srv://mcvianycodes:Engineering2k@cluster0.urtdp8f.mongodb.net/?retryWrites=true&w=majority');
+}
+// const db = mongoose.Connection;
+
+const db = connectTodb()
 // Express Routes Import
 const AuthorizationRoutes = require("./authorization/routes");
 const UserRoutes = require("./users/routes");
@@ -24,21 +31,21 @@ app.use(cors());
 // of middlewares and controllers.
 app.use(Express.json());
 
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "./storage/data.db", // Path to the file that will store the SQLite DB.
-});
+// const sequelize = new Sequelize({
+//   dialect: "sqlite",
+//   storage: "./storage/data.db", // Path to the file that will store the SQLite DB.
+// });
 
 // Initialising the Model on sequelize
-UserModel.initialise(sequelize);
-ProductModel.initialise(sequelize);
+UserModel.initialise(mongoose);
+ProductModel.initialise(mongoose);
 
 // Syncing the models that are defined on sequelize with the tables that alredy exists
 // in the database. It creates models as tables that do not exist in the DB.
-sequelize
-  .sync()
-  .then(() => {
-    console.log("Sequelize Initialised!!");
+// db
+//   .sync()
+  connectTodb().then(() => {
+    console.log("Mongoose is  Initialised!!");
 
     // Attaching the Authentication and User Routes to the app.
     app.use("/", AuthorizationRoutes);
@@ -54,4 +61,15 @@ sequelize
   });
 
 
+  // db.on('error', console.error.bind(console, 'MONGODB CONNECTION ERROR: '));
+  // db.once('open', () => {
+  //   console.log('CONNECTED TO MONGODB')
+  //   // Attaching the Authentication and User Routes to the app.
+  //   app.use("/", AuthorizationRoutes);
+  //   app.use("/user", UserRoutes);
+  //   app.use("/product", ProductRoutes);
 
+  //   app.listen(PORT, () => {
+  //     console.log("Server Listening on PORT:", port);
+  //   });
+  // });
