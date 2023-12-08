3e8eba904dc6d008a2f4d676f432402b53868ac1
noklash <123834419+noklash@users.noreply.github.com>
2023-12-08 11:54:43 +0300

    order route

--- orders/routes.js
+++ orders/routes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const Order = require("../../common/models/Order");
 
 
 const isAuthenticatedMiddleware = require("./../common/middlewares/IsAuthenticatedMiddleware");
