d9a360110ad29f08e4c9cc2bcc07b177a3dcfefc
noklash <123834419+noklash@users.noreply.github.com>
2024-01-08 16:41:32 +0100

    added the tokens

--- nodemailer/mailing.js
+++ nodemailer/mailing.js
@@ -3,18 +3,18 @@ const { google } = require('googleapis');
 
 const sendingMail = async ({ from, to, subject, text }) => {
     try {
-        const oauth2Client = new google.auth.OAuth2(
-            process.env.CLIENT_ID,
-            process.env.CLIENT_SECRET,
-            // "https://developers.google.com/oauthplayground" // Redirect URI, you can set it to OAuth 2.0 Playground for testing
-            "http://127.0.0.1:3000/oauth"
-            );
+        // const oauth2Client = new google.auth.OAuth2(
+        //     process.env.CLIENT_ID,
+        //     process.env.CLIENT_SECRET,
+        //     // "https://developers.google.com/oauthplayground" // Redirect URI, you can set it to OAuth 2.0 Playground for testing
+        //     "http://127.0.0.1:3000/oauth"
+        //     );
 
         // oauth2Client.setCredentials({
         //     refresh_token: process.env.REFRESH_TOKEN,
         // });
 
-        const accessToken = await oauth2Client.getAccessToken();
+        // const accessToken = await oauth2Client.getAccessToken();
 
         const transporter = nodemailer.createTransport({
             service: 'gmail',
@@ -23,8 +23,8 @@ const sendingMail = async ({ from, to, subject, text }) => {
                 user: 'njokugoodluck2k@gmail.com',
                 clientId: process.env.CLIENT_ID,
                 clientSecret: process.env.CLIENT_SECRET,
-                // refreshToken: process.env.REFRESH_TOKEN,
-                accessToken: accessToken,
+                refreshToken: process.env.REFRESH_TOKEN,
+                accessToken: process.env.ACCESS_TOKEN
             },
         });
 
